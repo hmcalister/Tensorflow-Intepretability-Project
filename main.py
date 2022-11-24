a78e0845bdac17ae8c4fe318bb8e43df2b575032
Hayden McAlister <mcaha814@student.otago.ac.nz>
2022-11-24 16:43:58 +1300

    Change main to use new MNIST classification task for testing

--- main.py
+++ main.py
@@ -12,50 +12,54 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import tensorflow as tf
 # fmt: on
 
-model_input_shape = (2,)
+print(f"GPU: {tf.config.list_physical_devices('GPU')}")
+model_input_shape = (28,28,1)
 
-# Feature names of iris dataset
-# Each new list will produce a new task with those labels as features
-# Possible labels are ['sepallength', 'sepalwidth', 'petallength', 'petalwidth']
-task_feature_names = [
-    ['sepallength', 'petallength'],
-    ['sepalwidth', 'petalwidth'],
-    ['petalwidth', 'sepallength'],
+# Digits to classify in each task
+# Should be taken from 0-9
+task_digit_classes = [
+    [0,1],
+    [2,3],
+    [4,5]
 ]
 
 # base model for sequential tasks
 # each model gets these layers as a base, then adds own head layers
 # i.e. these weights are *shared*
 base_model_inputs = base_model_layer = tf.keras.Input(shape=model_input_shape)
-base_model_layer = tf.keras.layers.Dense(64, activation="relu")(base_model_layer)
-base_model_layer = tf.keras.layers.Dense(32, activation="relu")(base_model_layer)
-base_model_layer = tf.keras.layers.Dense(32, activation="relu")(base_model_layer)
-# base_model_layer = tf.keras.layers.Dense(1)(base_model_layer)
+base_model_layer = tf.keras.layers.Conv2D(10, (5,5), activation="relu")(base_model_layer)
+base_model_layer = tf.keras.layers.MaxPool2D((3,3))(base_model_layer)
+base_model_layer = tf.keras.layers.Conv2D(10, (3,3), activation="relu")(base_model_layer)
+base_model_layer = tf.keras.layers.Flatten()(base_model_layer)
+base_model_layer = tf.keras.layers.Dense(10, activation="relu")(base_model_layer)
 base_model = tf.keras.Model(inputs=base_model_inputs, outputs=base_model_layer, name="base_model")
 
 # Layers specific to each task
 # Not shared
 task_head_layers = [
     [
-        tf.keras.layers.Dense(3)
+        tf.keras.layers.Dense(2, activation="softmax")
     ],
     [
-        tf.keras.layers.Dense(3)
+        tf.keras.layers.Dense(2, activation="softmax")
     ],
     [
-        tf.keras.layers.Dense(3)
-    ]
+        tf.keras.layers.Dense(2, activation="softmax")
+    ],
 ]
 
 # The base loss function for tasks
 # Currently all tasks have the same structure so only one loss
 # Could use a list in future
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 loss_fn.name = "base_loss"
 
 # Training parameters
 epochs = 50
-ewc_method = EWC_Method.NONE
+training_batches = 300
+validation_batches = 100
+batch_size = 32
+ewc_method = EWC_Method.WEIGHT_DECAY
 
 
 print(f"BASE MODEL SUMMARY")
@@ -67,8 +71,8 @@ base_model.summary()
 
 # Create, compile, and build all models
 models = []
-for task_index in range(len(task_feature_names)):
-    if task_index >= len(task_head_layers):
+for task_index in range(len(task_digit_classes)):
+    if task_head_layers is None:
         layers = []
     else:
         layers = task_head_layers[task_index]
@@ -83,12 +87,15 @@ for task_index in range(len(task_feature_names)):
 
 # Create the task representations (see SequentialTask)
 tasks = []
-for task_index in range(len(task_feature_names)):
-    tasks.append(IrisClassificationTask(
+for task_index in range(len(task_digit_classes)):
+    tasks.append(MNISTClassificationTask(
         name=f"Task {task_index+1}",
         model=models[task_index],
         model_base_loss=loss_fn,
-        feature_column_names=task_feature_names[task_index],
+        task_digit_labels=task_digit_classes[task_index],
+        training_batches = training_batches,
+        validation_batches = validation_batches,
+        batch_size=batch_size
     ))
 
 
@@ -98,7 +105,7 @@ manager = SequentialLearningManager(base_model, tasks, epochs, ewc_method)
 manager.train_all()
 # Plot output data
 manager.plot_validation_callback_data(
-    "loss", title="Task Total Loss Over All Epochs", ylabel="Total Loss")
+    "loss", title="Task Total Loss Over All Epochs", ylabel="Total Loss (CategoricalCrossentropy)")
 manager.plot_validation_callback_data(
-    "base_loss", title="Task Base Losses Over All Epochs", ylabel="Base Loss")
+    "base_loss", title="Task Base Losses Over All Epochs", ylabel="Base Loss (CategoricalCrossentropy)")
 manager.plot_task_training_histories()
