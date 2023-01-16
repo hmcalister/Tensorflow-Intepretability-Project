f7468b407e0aa9862421b7fe90c693554d981dca
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-01-16 16:09:07 +1300

    Update setup to better describe module

--- Utilities/setup.py
+++ Utilities/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='Utilities',
       version='1.0',
-      description='Sequential Learning Library using Tensorflow',
+      description='Sequential Learning  and Interpretability Library using Tensorflow',
       author='Hayden McAlister',
       author_email='mcaha814@student.otago.ac.nz',
       packages=["SequentialLearning","Interpretability"],
